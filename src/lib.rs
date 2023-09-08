dbd7133711a8a8eedec8bea3836feccc1ea9d09f
Wafol <61931496+Wafol@users.noreply.github.com>
2023-09-08 10:38:51 +0200

    try with my own www

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,43 @@
 // The wasm-pack uses wasm-bindgen to build and generate JavaScript binding file.
 // Import the wasm-bindgen crate.
 use wasm_bindgen::prelude::*;
+use web_sys::HtmlCanvasElement;
 
-// Our Add function
-// wasm-pack requires "exported" functions
-// to include #[wasm_bindgen]
+mod func_plot;
+
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+/// Type alias for the result of a drawing function.
+pub type DrawResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
   return a + b;
+}
+
+
+
+
+
+#[wasm_bindgen]
+pub struct Chart {
+    convert: Box<dyn Fn((i32, i32)) -> Option<(f64, f64)>>,
+}
+
+
+#[wasm_bindgen]
+pub fn power(canvas_id: &str, power: i32) -> Result<Chart, JsValue> {
+    let map_coord = func_plot::draw(canvas_id, power).map_err(|err| err.to_string())?;
+    Ok(Chart {
+        convert: Box::new(move |coord| map_coord(coord).map(|(x, y)| (x.into(), y.into()))),
+    })
+}
+
+
+#[wasm_bindgen]
+impl Chart {
+    
 }
--- www/index.js
+++ www/index.js
@@ -2,6 +2,25 @@
 // Which, export default's, an initialization function
 import init from "../pkg/BacteriaGraphing.js";
 
+
+
+const canvas = document.getElementById("canvas");
+
+
+
+function setupCanvas() {
+	const dpr = window.devicePixelRatio || 1.0;
+  const aspectRatio = canvas.width / canvas.height;
+  const size = canvas.parentNode.offsetWidth * 0.8;
+  canvas.style.width = size + "px";
+  canvas.style.height = size / aspectRatio + "px";
+  canvas.width = size;
+  canvas.height = size / aspectRatio;
+  
+  updatePlot();
+}
+
+
 const runWasm = async () => {
   // Instantiate our wasm module
   const bacteriaGraphing = await init("../pkg/BacteriaGraphing_bg.wasm");
@@ -13,4 +32,29 @@ const runWasm = async () => {
   document.body.textContent = `Hello World! addResult: ${addResult}`;
 };
 
-runWasm();
+const updatePlot = async () => {
+  const selected = plotType.selectedOptions[0];
+  status.innerText = `Rendering ${selected.innerText}...`;
+  chart = null;
+  const start = performance.now();
+
+  
+  const bacteriaGraphing = await init("../pkg/BacteriaGraphing_bg.wasm");
+
+
+  control.classList.add("hide");
+  chart = bacteriaGraphing.power("canvas", Number(selected.value))
+
+
+  const end = performance.now();
+  status.innerText = `Rendered ${selected.innerText} in ${Math.ceil(end - start)}ms`;
+}
+
+
+
+
+setupCanvas();
+
+updatePlot();
+
+//runWasm();
